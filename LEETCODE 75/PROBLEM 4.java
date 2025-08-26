2fa2fbabe5c1bf59f7b1465921aa874188dbc9b6
VIKASHINI SIVASELVAMOORTHI <167671299+VIKASHINI14092004@users.noreply.github.com>
2025-08-26 16:19:29 +0530

    Create PROBLEM 4.java

---
+++ LEETCODE 75/PROBLEM 4.java
@@ -0,0 +1,61 @@
+/*PROBLEM STATEMENT
+CAN PLACE FLOWERS
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, 
+return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise. 
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+Constraints:
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+
+SOLUTION */
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) 
+    {
+        int n1=flowerbed.length;
+        int count=0;
+        boolean leftempty=false;
+        boolean rightempty=false;
+        for(int i=0;i<n1;i++)
+        {
+            leftempty=false;
+            rightempty=false;
+            if(flowerbed[i]==0)
+            {
+
+            if((i==0)||(flowerbed[i-1]==0))
+            {
+                leftempty=true;
+            }
+
+            if((i==n1-1)||flowerbed[i+1]==0)
+            {
+                rightempty=true;
+            }
+            if((leftempty==true)&&(rightempty==true))
+            {
+                flowerbed[i]=1;
+                count++;
+            }
+            }
+        }
+        if(count>=n)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
