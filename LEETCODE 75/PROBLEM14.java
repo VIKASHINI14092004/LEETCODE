8c277770351125a66fd3020a20792b83ad5309b3
VIKASHINI SIVASELVAMOORTHI <167671299+VIKASHINI14092004@users.noreply.github.com>
2025-08-29 09:53:40 +0530

    Update and rename 14.MAXIMUM_AVG_SUBARRAY_USING_SLIDING_WINDOW.java to PROBLEM14.java

--- LEETCODE 75/PROBLEM14.java
+++ LEETCODE 75/PROBLEM14.java
@@ -1,5 +1,6 @@
 /*
 PROBLEM STATEMENT:
+MAXIMUM_AVG_SUBARRAY_USING_SLIDING_WINDOW
 You are given an integer array nums consisting of n elements, and an integer k.
 Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
 Any answer with a calculation error less than 10-5 will be accepted.
