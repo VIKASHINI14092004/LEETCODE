987f231cb67f4b9805f0005cd80c5ebdbfa665d0
VIKASHINI SIVASELVAMOORTHI <167671299+VIKASHINI14092004@users.noreply.github.com>
2025-09-17 20:37:56 +0530

    Create PROBLEM26.java

---
+++ LEETCODE 75/PROBLEM26.java
@@ -0,0 +1,71 @@
+/*
+PROBLEM STATEMENT
+DECODE THE STRING
+Given an encoded string, return its decoded string.
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+Note that k is guaranteed to be a positive integer.
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. 
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+For example, there will not be input like 3a or 2[4].
+The test cases are generated so that the length of the output will never exceed 105.
+
+Example 1:
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+  
+Example 2:
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+  
+Example 3:
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+ 
+
+Constraints:
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+
+SOLUTION */
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> nocount=new Stack<>();
+        Stack<String> prevStack=new Stack<>();
+        String current="";
+        int k=0;
+        for(char ch:s.toCharArray())
+        {
+            if(Character.isDigit(ch))
+            {
+                k=k*10+(ch-'0');
+            }
+            else if(ch=='[')
+            {
+                nocount.push(k);
+                prevStack.push(current);
+                k=0;
+                current="";
+            }
+            else if(ch==']')
+            {
+                int n=nocount.pop();
+                String prev=prevStack.pop();
+                StringBuilder sb=new StringBuilder(prev);
+                for(int i=0;i<n;i++)
+                {
+                    sb.append(current);
+                }
+                current =sb.toString();
+            }
+            else
+            {
+                current+=ch;
+            }
+
+
+        }
+        return current;
+    }
+}
